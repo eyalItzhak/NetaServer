cc99d7d178bd4375d4da67075240dc673a1661e9
eyalItzhak <eyalpross101@gmail.com>
2024-07-08 23:07:58 +0300

    build client on servere

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { json } from "body-parser";
 import { roomsRouter } from "./routes/rooms";
 import { connectToSocket } from "./socket/socket";
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
@@ -9,6 +10,10 @@ const app = express();
 const server = http.createServer(app);
 
 connectToSocket(server);
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req: any, res: any) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(cors());
 app.use(json());
